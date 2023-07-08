5c8e1e279fb7cc98dcdeb92e5597e3268dd43c16
yxoni <90109410+yxoni@users.noreply.github.com>
2023-07-09 02:54:00 +0900

    Feat: 기능 추가

--- js/pricecomparison.js
+++ js/pricecomparison.js
@@ -1,3 +1,40 @@
+/* 나라 선택 */
+var jsonLocation = "../json/countryList.json";
+$.getJSON(jsonLocation, function(data) {
+    const length = data.data.length;
+
+    var i;
+    for (i = 0; i < length; i++) {
+
+        const countryName = data.data[i].countryName;
+
+        let countryList = document.querySelector(".country1");
+        let country = document.createElement("option");
+        country.setAttribute("value", "US");
+        country.innerHTML = countryName;
+        countryList.appendChild(country);
+    }
+});
+
+/* 지역 선택 */
+var jsonLocation = "../json/divisionList.json";
+$.getJSON(jsonLocation, function(data) {
+    const length = data.data.length;
+
+    var i;
+    for (i = 0; i < length; i++) {
+
+        const ADName = data.data[i].ADName;
+
+        let divisionList = document.querySelector(".division1");
+        let division = document.createElement("option");
+        division.setAttribute("value", "US");
+        division.innerHTML = ADName;
+        divisionList.appendChild(division);
+    }
+});
+
+
 var barChartData = {
     labels: ["한끼 평균 식사비", "1km당 평균 택시비", "한 잔 평균 커피값", "각종 먹거리"],
     datasets: [{
