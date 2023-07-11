45f6d02e1c35cef186a888ddd0291551d4220ca6
yxoni <90109410+yxoni@users.noreply.github.com>
2023-07-12 02:59:02 +0900

    Feat: 마이페이지 수정 기능

--- js/mypageMod.js
+++ js/mypageMod.js
@@ -6,8 +6,10 @@ $.getJSON(jsonLocation, function(data) {
 
         const userName = data.data.userName;
         const email = data.data.email;
-        const nickName = data.data.nickName;
-        // const picture = data.data.picture;
+        const nick = data.data.nickName;
+        const id = data.data.userId;
+        const pw = data.data.userPw;
+        const picture = data.data.picture;
 
         let wrap_infor = document.querySelector(".wrap_infor");
 
@@ -16,59 +18,67 @@ $.getJSON(jsonLocation, function(data) {
         // img.src = picture;
 
         /* 이름 */
-        let input_name = document.querySelector(".input_name");
+        var input_name = document.querySelector(".input_name");
         input_name.setAttribute("value", userName)
 
-        let input_email = document.querySelector(".input_email");
+        var input_email = document.querySelector(".input_email");
         input_email.setAttribute("value", email)
 
-        let input_nickName = document.querySelector(".input_nick");
-        input_nickName.setAttribute("value", nickName)
+        var input_nickName = document.querySelector(".input_nick");
+        input_nickName.setAttribute("value", nick)
+
+        var input_id = document.querySelector(".input_id");
+        input_id.setAttribute("value", id)
+
+        var input_pw = document.querySelector(".input_pw");
+        input_pw.setAttribute("value", pw)
 
         // wrap_infor.appendChild(img); // 없어서 보류
-
-
     }
     showData();
-
 });
 
+$('#picture').on('click', function() {
+    $('#picture_input').click();
+});
 
+$('#picture_input').on('change', function() {
+    var file = this.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#picture').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(file);
+    }
+})
 
+const submit = () => {
+    var profilePictureFile = $('#picture_input')[0].files[0];
 
-// /*** 회원 정보 ***/
-// var jsonLocation = "../json/userInfor.json";
-// $.getJSON(jsonLocation, function(data) {
+    var updatedDate = {
+        "userPw": $('input_id.value').val(),
+        "userId": $('input_pw.value').val(),
+        "picture": profilePictureFile,
+        "nickName": $('input_nick.value').val()
+    }
+    sendUpdateRequest(updatedDate);
+}
 
-//     const showData = () => {
-
-//         const userName = data.data.userName;
-//         const email = data.data.email;
-//         const nickName = data.data.nickName;
-//         // const picture = data.data.picture;
-
-//         let wrap_infor = document.querySelector(".wrap_infor");
-
-//         /* 이미지 */
-//         // let img = document.createElement("img");
-//         // img.src = picture;
-
-//         /* 이름 */
-//         let p_name = document.createElement("p");
-//         p_name.innerHTML = userName;
-//         let p_email = document.createElement("p");
-//         p_email.setAttribute("class", "memEmail");
-//         p_email.innerHTML = email;
-//         let p_nickName = document.createElement("p");
-//         p_nickName.setAttribute("class", "memNick");
-//         p_nickName.innerHTML = nickName;
-
-//         // wrap_infor.appendChild(img); // 없어서 보류
-//         wrap_infor.appendChild(p_name);
-//         wrap_infor.appendChild(p_email);
-//         wrap_infor.appendChild(p_nickName);
-
-//     }
-//     showData();
-
-// });
+const sendUpdateRequest = (updatedDate) => {
+    $.ajax({
+        type: 'PUT',
+        url: 'http://grishare.ap-northeast-2.elasticbeanstalk.com/api/mypage',
+        contentType: 'application/json',
+        data: JSON.stringify(updatedDate),
+        success: function() {
+            alert('통신 성공시에만 실행');
+            console.log("성공");
+        },
+        error: function(request, status, error) {
+            console.log("code: " + request.status)
+            console.log("message: " + request.responseText)
+            console.log("error: " + error);
+        }
+    });
+}
--- js/pricecomparison.js
+++ js/pricecomparison.js
@@ -1,4 +1,80 @@
 /* 나라 보여주기 */
+
+// $.ajax({
+//     type: 'GET',
+//     url: 'http://grishare.ap-northeast-2.elasticbeanstalk.com/api/compare/country',
+//     success: function() {
+//         alert('통신 성공시에만 실행');
+//         console.log("성공");
+//     },
+//     error: function() {
+//         alert('통신 실패시에만 실행');
+//         console.log("왜 실패?");
+//     }
+// });
+
+var average1 = 100000000;
+var average2 = 100000000;
+var average3 = 100000000;
+
+var wrap_cheap = document.querySelector(".wrap_cheap");
+
+const averageCalc = () => {
+
+    /* 나라 html + css */
+    let country1 = document.createElement("p");
+    country1.setAttribute("class", "cheapCoun");
+    country1.innerHTML = "나라1";
+
+    let country2 = document.createElement("p");
+    country2.setAttribute("class", "cheapCoun");
+    country2.innerHTML = "나라2";
+
+    let country3 = document.createElement("p");
+    country3.setAttribute("class", "cheapCoun");
+    country3.innerHTML = "나라3";
+
+    var cheacpCountry = 0;
+
+    cheacpCountry = ((average1 < average2) && (average1 < average3)) ? average1 : (((average2 < average3) ? average2 : average3));
+
+    console.log(average1);
+    console.log(average2);
+    console.log(average3);
+
+    if (cheacpCountry == average1) {
+        if (average1 == average2) {
+            if (average2 == average3) {
+                wrap_cheap.appendChild(country1);
+                wrap_cheap.appendChild(country2);
+                wrap_cheap.appendChild(country3);
+            } else {
+                wrap_cheap.appendChild(country1);
+                wrap_cheap.appendChild(country2);
+            }
+        } else if (average1 == average3) {
+            wrap_cheap.appendChild(country1);
+            wrap_cheap.appendChild(country3);
+        } else {
+            wrap_cheap.appendChild(country1);
+
+        }
+    } else if (cheacpCountry == average2) {
+        if (average2 == average3) {
+            wrap_cheap.appendChild(country2);
+            wrap_cheap.appendChild(country3);
+        } else {
+            wrap_cheap.appendChild(country2);
+        }
+    } else if (cheacpCountry == average3) {
+        wrap_cheap.appendChild(country3);
+
+    }
+}
+
+/********** 나라 **********/
+
+/* 나라 보여주기 1 */
 var jsonLocation = "../json/countryList.json";
 $.getJSON(jsonLocation, function(data) {
     const length = data.data.length;
@@ -17,13 +93,22 @@ $.getJSON(jsonLocation, function(data) {
     }
 });
 
-var dropdownC = document.getElementById("country1");
+var dropdownC1 = document.getElementById("country1");
+dropdownC1.addEventListener("change", function() {
+    if (wrap_cheap.childElementCount > 0)
+        wrap_cheap.replaceChild();
 
-dropdownC.addEventListener("change", function() {
+    /* 차트 초기화 */
+
+    barChartData.datasets[0].data[0] = 0;
+    barChartData.datasets[0].data[1] = 0;
+    barChartData.datasets[0].data[2] = 0;
+    barChartData.datasets[0].data[3] = 0;
+    window.theChart.update();
 
     /* 지역 찾기 */
 
-    selectedOption = dropdownC.value;
+    selectedOption = dropdownC1.value;
 
     var find1 = "../json/";
     var find2 = ".json";
@@ -48,27 +133,144 @@ dropdownC.addEventListener("change", function() {
     });
 });
 
-/* 물가 찾기 */
+/* 나라 보여주기 2 */
+var jsonLocation = "../json/countryList.json";
+$.getJSON(jsonLocation, function(data) {
+    const length = data.data.length;
 
-var dropdownD = document.getElementById("division1");
+    var i;
+    for (i = 0; i < length; i++) {
 
-dropdownD.addEventListener("change", function() {
+        const countryName = data.data[i].countryName;
+        const code = data.data[i].code;
 
-    selectedOption = dropdownD.value;
+        let countryList = document.querySelector(".country2");
+        let country = document.createElement("option");
+        country.setAttribute("value", code);
+        country.innerHTML = countryName;
+        countryList.appendChild(country);
+    }
+});
+
+var dropdownC2 = document.getElementById("country2");
+dropdownC2.addEventListener("change", function() {
+    if (wrap_cheap.childElementCount > 0)
+        wrap_cheap.replaceChild();
+
+    /* 차트 초기화 */
+
+    barChartData.datasets[1].data[0] = 0;
+    barChartData.datasets[1].data[1] = 0;
+    barChartData.datasets[1].data[2] = 0;
+    barChartData.datasets[1].data[3] = 0;
+    window.theChart.update();
+
+    /* 지역 찾기 */
+
+    selectedOption = dropdownC2.value;
 
     var find1 = "../json/";
     var find2 = ".json";
     var findPath = find1 + selectedOption + find2;
 
-    console.log(findPath);
+    var jsonLocation = findPath;
+    $.getJSON(jsonLocation, function(data) {
+        const length = data.data.length;
+
+        var i;
+        for (i = 0; i < length; i++) {
+
+            const ADName = data.data[i].ADName;
+            const code = data.data[i].code;
+
+            let divisionList = document.querySelector(".division2");
+            let division = document.createElement("option");
+            division.setAttribute("value", code);
+            division.innerHTML = ADName;
+            divisionList.appendChild(division);
+        }
+    });
+});
+
+/* 나라 보여주기 3 */
+var jsonLocation = "../json/countryList.json";
+$.getJSON(jsonLocation, function(data) {
+    const length = data.data.length;
+
+    var i;
+    for (i = 0; i < length; i++) {
+
+        const countryName = data.data[i].countryName;
+        const code = data.data[i].code;
+
+        let countryList = document.querySelector(".country3");
+        let country = document.createElement("option");
+        country.setAttribute("value", code);
+        country.innerHTML = countryName;
+        countryList.appendChild(country);
+    }
+});
+
+var dropdownC3 = document.getElementById("country3");
+dropdownC3.addEventListener("change", function() {
+    if (wrap_cheap.childElementCount > 0)
+        wrap_cheap.replaceChild();
+
+    /* 차트 초기화 */
+
+    barChartData.datasets[2].data[0] = 0;
+    barChartData.datasets[2].data[1] = 0;
+    barChartData.datasets[2].data[2] = 0;
+    barChartData.datasets[2].data[3] = 0;
+    window.theChart.update();
+
+    /* 지역 찾기 */
+
+    selectedOption = dropdownC3.value;
+
+    var find1 = "../json/";
+    var find2 = ".json";
+    var findPath = find1 + selectedOption + find2;
+
+    var jsonLocation = findPath;
+    $.getJSON(jsonLocation, function(data) {
+        const length = data.data.length;
+
+        var i;
+        for (i = 0; i < length; i++) {
+
+            const ADName = data.data[i].ADName;
+            const code = data.data[i].code;
+
+            let divisionList = document.querySelector(".division3");
+            let division = document.createElement("option");
+            division.setAttribute("value", code);
+            division.innerHTML = ADName;
+            divisionList.appendChild(division);
+        }
+    });
+});
+
+
+/********** 물가 **********/
+
+/* 물가 찾기1 */
+var dropdownD1 = document.getElementById("division1");
+dropdownD1.addEventListener("change", function() {
+
+    selectedOption = dropdownD1.value;
+
+    var find1 = "../json/";
+    var find2 = ".json";
+    var findPath = find1 + selectedOption + find2;
 
     var jsonLocation = findPath;
     $.getJSON(jsonLocation, function(data) {
 
-        const meal = data.data.meal;
-        const transport = data.data.transport;
-        const accom = data.data.accom;
-        const coffee = data.data.coffee;
+        var meal = data.data.meal;
+        var transport = data.data.transport;
+        var accom = data.data.accom;
+        var coffee = data.data.coffee;
 
         barChartData.datasets[0].data[0] = meal;
         barChartData.datasets[0].data[1] = transport;
@@ -76,13 +278,74 @@ dropdownD.addEventListener("change", function() {
         barChartData.datasets[0].data[3] = coffee;
         window.theChart.update();
 
+        average1 = (Number(meal) + Number(transport) + Number(accom) + Number(coffee)) / 4;
+
+        averageCalc();
     });
-
-
-
-
-
 });
+
+/* 물가 찾기2 */
+var dropdownD2 = document.getElementById("division2");
+dropdownD2.addEventListener("change", function() {
+
+    selectedOption = dropdownD2.value;
+
+    var find1 = "../json/";
+    var find2 = ".json";
+    var findPath = find1 + selectedOption + find2;
+
+    var jsonLocation = findPath;
+    $.getJSON(jsonLocation, function(data) {
+
+        var meal = data.data.meal;
+        var transport = data.data.transport;
+        var accom = data.data.accom;
+        var coffee = data.data.coffee;
+
+        barChartData.datasets[1].data[0] = meal;
+        barChartData.datasets[1].data[1] = transport;
+        barChartData.datasets[1].data[2] = accom;
+        barChartData.datasets[1].data[3] = coffee;
+        window.theChart.update();
+
+        average2 = (Number(meal) + Number(transport) + Number(accom) + Number(coffee)) / 4;
+
+        averageCalc();
+    });
+});
+
+/* 물가 찾기3 */
+var dropdownD3 = document.getElementById("division3");
+dropdownD3.addEventListener("change", function() {
+
+    selectedOption = dropdownD3.value;
+
+    var find1 = "../json/";
+    var find2 = ".json";
+    var findPath = find1 + selectedOption + find2;
+
+    var jsonLocation = findPath;
+    $.getJSON(jsonLocation, function(data) {
+
+        var meal = data.data.meal;
+        var transport = data.data.transport;
+        var accom = data.data.accom;
+        var coffee = data.data.coffee;
+
+        barChartData.datasets[2].data[0] = meal;
+        barChartData.datasets[2].data[1] = transport;
+        barChartData.datasets[2].data[2] = accom;
+        barChartData.datasets[2].data[3] = coffee;
+        window.theChart.update();
+
+        average3 = (Number(meal) + Number(transport) + Number(accom) + Number(coffee)) / 4;
+
+        averageCalc();
+    });
+});
+
+
+/********** 차트 **********/
 
 /* 차트 값 */
 var barChartData = {
